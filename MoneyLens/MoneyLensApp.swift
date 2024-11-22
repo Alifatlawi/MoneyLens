0c346e90309e9651e605a48ee698635b13103d2f
Ali <alawi_amer@yahoo.co.uk>
2024-11-22 23:22:24 +0300

    Initial Commit

---
+++ MoneyLens/MoneyLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyLensApp.swift
+//  MoneyLens
+//
+//  Created by BLG-BC-018 on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoneyLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
